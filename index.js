dd5ecda08c8d1474be8d6ec234d86e04fcfa59f1
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-10 20:30:02 -0800

    server is running

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // require your server and launch it
+const server = require('./api/server')
+const port = 9090
+
+server.listen(port, () => {
+    console.log(` Server is listnening of ${port}`)
+})
