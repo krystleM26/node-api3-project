21c898af11118fb19db6b6d8bfc76b27486c4b2a
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-10 21:19:23 -0800

    Logger commits

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,32 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+
+  console.log('Logger')
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  next()
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
